c62eae86abfedbf92918c7f66cef323d1d4c0afb
EmmanuelSkapple <emgarcia@hotmail.com>
2018-08-16 12:18:31 -0500

    landing basica

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Exemplar from './Exemplar';
+import Features from './Features';
+import About from './About';
+import LatestNews from './LatestNews';
+import SocialBrand from './SocialBrand';
+import Footer from './Footer';
+import '../index.css';
+
+const App = () => {
+  return (
+    <div id="app">
+      <Navbar />
+      <Hero />
+      <Exemplar />
+      <Features />
+      <About />
+      <LatestNews />
+      <SocialBrand />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
